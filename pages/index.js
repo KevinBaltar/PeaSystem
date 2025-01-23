3aeff0a27f380a170b7878c8a3f351f8b3f42e0a
Anderson Kevin Araújo Baltar <150495042+KevinBaltar@users.noreply.github.com>
2025-01-23 13:40:47 +0000

    corrige a frase

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-    return <h1>Fernanda Baltar. Eu te amo!Se você tambem me ama,dê um sorriso(:D)!</h1>
+    return <h1>Fernanda Baltar. Eu te amo!Se você também me ama,dê um sorriso(:D)!</h1>
 }
 export  default Home;
